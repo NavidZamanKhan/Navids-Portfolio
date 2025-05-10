60dcc99d1eec08d622f0a1f9f16dae4c4fb7a27b
NavidZamanKhan <navid166418@gmail.com>
2025-05-10 15:29:53 +0600

    added home page containers on the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:navids_portfolio/screens/home_page.dart';
+
+final colorScheme = ColorScheme.fromSeed(
+  seedColor: Colors.orangeAccent,
+  brightness: Brightness.dark,
+);
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: colorScheme,
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
   runApp(const MyWebApp());
@@ -8,8 +21,6 @@ class MyWebApp extends StatelessWidget {
   const MyWebApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text("Hello World"))),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
